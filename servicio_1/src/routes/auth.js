8449f6d6277ba4d2132bc8b47c463ba748571e25
DiegoLaguna-17 <diego.laguna@ucb.edu.bo>
2026-05-26 20:34:20 -0400

    Servicio de manejo de usuarios

--- servicio_1/src/routes/auth.js
+++ servicio_1/src/routes/auth.js
@@ -2,8 +2,16 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 
-router.post('/register', authController.register);
+router.post('/registrar/paciente', authController.registrarPaciente);
 router.post('/login', authController.login);
 router.get('/health', authController.health);
+router.post('/registrar/medico', authController.registrarMedico);
+
+router.post('/agregar/rol', authController.agregarRol);
+
+router.post('/registrar/farmacia',authController.registrarFarmacia)
+router.post('/registrar/personal/farmacia',authController.registrarPersonalFarmacia)
+router.post('/registrar/personal/salud',authController.registrarPersonalSalud)
+router.post('/registrar/auditor',authController.registrarAuditor)
 
 module.exports = router;
